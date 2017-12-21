0ce0035ee8d4386431811ab61c0b6dd492725af3
Adam McCurdy <adam@FHI-Ubuntu.xgiva23p5ikepbqvf3qtnbcnhg.yx.internal.cloudapp.net>
2017-12-21 23:28:43 +0000

    Updates to display and dayrank()

--- normalShiny/R/elementRankMod.R
+++ normalShiny/R/elementRankMod.R
@@ -23,21 +23,32 @@ elementRank <- function(input, output, session, weatherData, myElement, myDate){
     print(class(myDate()))
     myData <- filteredWeather()[month == month(myDate()) &
                                day == mday(myDate())
-                               ,][order(value), value]
+                               ,][order(value, decreasing = T), value]
     myValue <- filteredWeather()[date == myDate(), value]
-    myReturn <- vector("list", 2)
-    names(myReturn) <- c("rank", "percentile")
+    myReturn <- vector("list", 3)
+    names(myReturn) <- c("rank", "percentile", "years")
     myReturn$rank <- max(which(myData == myValue, arr.ind = T))
-    myReturn$percentile <- sum(myData <= myValue) / 
-                            length(myData)
+    myReturn$percentile <- round((sum(myData <= myValue) / 
+                            length(myData)) * 100)
+    myReturn$years <- length(myData)
     return(myReturn)
   })
   
   output$info <- renderUI({
-    tagList(
-      h5("Your date was greater than"),
-      h5(dayRank())
-    )
+    if(myElement %in% c("TMAX", "TMIN")){
+      tagList(
+        h5(paste0("The ", myElement, " was the ", dayRank()$rank, 
+                  " warmest of the past ", dayRank()$years, " years")),
+        h5(paste0("In other words it was warmer than ", dayRank()$percentile,
+                  " % of years"))
+      )  
+    }else{
+      tagList(
+        h5("Your date was greater than"),
+        h5(dayRank())
+      )
+    }
+    
   })
   
 }
--- normalShiny/ui.R
+++ normalShiny/ui.R
@@ -25,7 +25,6 @@ shinyUI(fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-      h4("hello"),
       rankUI("TMAX")
     )
   )
