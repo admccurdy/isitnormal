feac6cc3928d40c58062613ab46ac8ab635bf68b
Adam McCurdy <adam@FHI-Ubuntu.xgiva23p5ikepbqvf3qtnbcnhg.yx.internal.cloudapp.net>
2018-01-23 17:15:47 +0000

    Updated .dly

--- R/dataBuilder.R
+++ R/dataBuilder.R
@@ -4,9 +4,8 @@ library(dplyr)
 library(rnoaa)
 
 
-ptm <- proc.time()
+
 stationList <- ghcnd_stations()
-ptm - proc.time()
 
 stationList <- stationList %>% data.table()
 
@@ -14,7 +13,8 @@ aspenStation <- stationList[longitude <= -106.41758 &
                               longitude >= -107.482302 &
                               latitude >= 38.879276 &
                               latitude <= 39.579248,]
-stationsInterest <- aspenStation[id %in% c("USC00050370", "USC00050372", "USS0006K04S", "USC00053359"),]
+stationsInterest <- aspenStation[id %in% c("USC00050370", "USC00050372", "USS0006K04S", "USC00053359",
+                                           "USW00093073"),]
 stationsInterest <- stationsInterest[element == "PRCP",]
 # saveRDS(myDailys, "normalShiny/data/dailys/dailys12_21.RDS")
 
@@ -25,8 +25,9 @@ myDailys <- rbindlist(myDailys)
 myDailys <- myDailys %>% left_join(stationsInterest[, c("id", "name")])
 myDailys <- myDailys %>% data.table()
 myDailys[, name := ifelse(name == "ASPEN 1SW", "ASPEN", name)]
-myDailys <- myDailys[element %in% c("TMAX", "TMIN", "PRCP", "WESD", "SNOW"),]
+myDailys <- myDailys[element %in% c("TMAX", "TMIN", "PRCP", "WESD", "SNOW", "TAVG"),]
 myDailys[, date := as.Date(paste0(year, "-", month, "-", day))]
+myDailys <- myDailys[!is.na(date),]
 saveRDS(myDailys, "normalShiny/data/dailys/currentDaily.RDS")
 
 
